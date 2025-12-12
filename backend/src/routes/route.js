151e0205530bd76aec98fe6101d35bd321f1c1d7
mikmak29 <mjogamaapple@gmail.com>
2025-12-12 13:10:52 +0800

    Multiple Router Added

--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -6,4 +6,16 @@ router.get("/", (req, res) => {
 	res.json({ message: "Server is working!" });
 });
 
+router.post("/", (req, res) => {
+	res.json({ message: "Created Data!" });
+});
+
+router.put("/:id", (req, res) => {
+	res.json({ message: `Updated Data ${req.params.id}` });
+});
+
+router.delete("/:id", (req, res) => {
+	res.json({ message: `Deleted Data ${req.params.id}` });
+});
+
 export default router;
